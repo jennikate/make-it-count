979c63126e57f5dd2cb8c7c977ddda4a40c40d6c
JenniKate Wallace <jennikate@gmail.com>
2019-11-21 11:39:36 +0000

    postcode error handling

--- src/components/PostcodeSearch.js
+++ src/components/PostcodeSearch.js
@@ -9,22 +9,25 @@ class PostcodeSearch extends React.Component {
       postcode: '',
       result: {
         parliamentary_constituency: ''
-      }
+      },
+      errors: ''
     }
   }
 
   handleUpdate(e) {
     this.setState({ postcode: e.target.value.toLowerCase().split(' ').join('') })
+    this.setState({ errors: '' })
   }
 
   handleSubmit(e) {
     e.preventDefault()
-    console.log(this.state.postcode)
     axios.get(`http://api.postcodes.io/postcodes/${this.state.postcode}`)
       .then(resp => this.setState({ parliamentary_constituency: resp.data.result.parliamentary_constituency }))
+      .catch(err => this.setState({ errors: err.response.data.error }))
   }
 
   render() {
+    console.log(this.state.parliamentary_constituency)
     return (
       <div className='field is-horizontal'>
         <div className='field-label is-normal'>
@@ -33,7 +36,10 @@ class PostcodeSearch extends React.Component {
         <div className='field-body'>
           <div className='field'>
             <p className='control'>
-              <input className='input' type='text' id='postcode' onChange={(e) => this.handleUpdate(e)} />
+              <input className='input' name='postcode' type='text' id='postcode' onChange={(e) => this.handleUpdate(e)} />
+              {this.state.errors && <small className='help is-danger'>
+                {this.state.errors}
+              </small>}
             </p>
           </div>
         </div>
