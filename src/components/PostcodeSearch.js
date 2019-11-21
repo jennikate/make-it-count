f15d5a7c59ce94f09d817a4b84feaceee5f85989
JenniKate Wallace <jennikate@gmail.com>
2019-11-21 11:26:49 +0000

    postcode lookup

--- src/components/PostcodeSearch.js
+++ src/components/PostcodeSearch.js
@@ -1,33 +1,43 @@
 import React from 'react'
+import axios from 'axios'
 
 
 class PostcodeSearch extends React.Component {
   constructor() {
     super()
     this.state = {
-      postcode: ''
+      postcode: '',
+      result: {
+        parliamentary_constituency: ''
+      }
     }
   }
 
   handleUpdate(e) {
-    console.log(e.target.value)
-    this.setState = e.target.value
+    this.setState({ postcode: e.target.value.toLowerCase().split(' ').join('') })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log(this.state.postcode)
+    axios.get(`http://api.postcodes.io/postcodes/${this.state.postcode}`)
+      .then(resp => this.setState({ parliamentary_constituency: resp.data.result.parliamentary_constituency }))
   }
 
   render() {
     return (
       <div className='field is-horizontal'>
         <div className='field-label is-normal'>
-          <label className='label' htmlFor='postcode' >Postcode</label>
+          <label className='label' htmlFor='postcode'>Postcode</label>
         </div>
         <div className='field-body'>
           <div className='field'>
             <p className='control'>
-              <input className='input' type='text' id='postcode' onChange={(e) => this.handleUpdate(e)}/>
+              <input className='input' type='text' id='postcode' onChange={(e) => this.handleUpdate(e)} />
             </p>
           </div>
         </div>
-        <button className='button'>Go</button>
+        <button className='button' onClick={(e) => this.handleSubmit(e)}>Go</button>
       </div>
     )
   }
