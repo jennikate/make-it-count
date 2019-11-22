c70dd28bf0931158de7b42185eb07cbf157df773
JenniKate Wallace <jennikate@gmail.com>
2019-11-22 14:01:26 +0000

    brief met

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
 
   return (
 
-    <footer className="footer">
+    <footer className="my-styles">
       <div className="content has-text-centered">
         <p>
           Data from <a href='https://democracyclub.org.uk' target='_blank' rel='noopener noreferrer'>Democracy Club</a>, <a href='https://postcodes.io/' target='_blank' rel='noopener noreferrer'>Postcodes.io</a>, <a href='http://www.data.parliament.uk/' target='_blank' rel='noopener noreferrer'>data.parliament.uk</a>
