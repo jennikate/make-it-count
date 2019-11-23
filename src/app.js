b7c1f43c3d2625932efd3afc6a73d5d2e6ea2dca
JenniKate Wallace <jennikate@gmail.com>
2019-11-23 12:33:27 +0000

    config

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import Constituency from './components/Constituency'
 const App = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path='/' component={Home} />
+      <Route exact path='/make-it-count' component={Home} />
       <Route exact path='/constituency/:id' component={Constituency} />
     </Switch>
   </BrowserRouter>
